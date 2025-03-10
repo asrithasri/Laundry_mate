a72d73a464da766fd8975c8899ec27a4a66c15af
asrithasri <asrithk133@gmail.com>
2025-03-10 13:35:42 +0530

    6 digit otp generates

--- src/auth/service-provider-auth.service.ts
+++ src/auth/service-provider-auth.service.ts
@@ -16,7 +16,7 @@ export class ServiceProviderAuthService {
   ) {}
 
   async generateOtp(): Promise<string> {
-    return Math.floor(10000 + Math.random() * 90000).toString(); // Generates a 5-digit OTP
+    return Math.floor(100000 + Math.random() * 900000).toString(); // Generates a 6-digit OTP
   }
 
   async loginOrRegister(loginDto: LoginDto): Promise<{ message: string; otp: string }> {
--- src/auth/user-auth.service.ts
+++ src/auth/user-auth.service.ts
@@ -17,7 +17,7 @@ export class UserAuthService {
 
 
   async generateotp(): Promise<string> {
-    return Math.floor(10000 + Math.random() * 90000).toString(); // Generates a 5-digit OTP
+    return Math.floor(100000 + Math.random() * 900000).toString(); // Generates a 6-digit OTP
   }
 
   async loginOrRegister(loginDto: LoginDto): Promise<{ message: string; otp: string }> {
